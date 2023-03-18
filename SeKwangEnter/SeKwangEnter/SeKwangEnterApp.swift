506a64f999f4d1444ea643825fa9a8f72ee166a0
Minny27 <dltmdals0608@naver.com>
2023-03-18 22:06:59 +0900

    init SEkwanEnter

---
+++ SeKwangEnter/SeKwangEnter/SeKwangEnterApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeKwangEnterApp.swift
+//  SeKwangEnter
+//
+//  Created by SeungMin on 2023/03/18.
+//
+
+import SwiftUI
+
+@main
+struct SeKwangEnterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
